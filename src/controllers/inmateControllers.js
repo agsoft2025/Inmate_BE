92881aeb964c0fb04fd64e82dc7bcd13aaab42f4
vipin760 <vipinm500@gmail.com>
2026-03-25 03:48:53 +0530

    log

--- src/controllers/inmateControllers.js
+++ src/controllers/inmateControllers.js
@@ -158,6 +158,7 @@ const createInmate = async (req, res) => {
     });
     res.status(201).json({ success: true, data: savedInmate, message: "Inmate successfully created" });
   } catch (error) {
+    console.log("<><>error",error);
     res.status(500).json({ success: false, message: "Internal server error", error: error.message });
   }
 };
